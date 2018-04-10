e3b7f1fd1fb193150ee13cf62ed0c1796c5df7bb
AnkushMalik <ankushmalik631@gmail.com>
2018-04-10 20:28:08 +0530

    add functions

--- reddipaper.rb
+++ reddipaper.rb
@@ -4,21 +4,27 @@ require 'nokogiri'
 
 redditscrape = Nokogiri::HTML(open('https://www.reddit.com/r/wallpapers/', 'User-Agent' => 'Nooby'))
 
-a = rand(0..redditscrape.css('.thing').size)
-wallobj = redditscrape.css('.thing')[a]
-wp_path = wallobj.css('.bylink').first.attributes['href'].value
-wppage = Nokogiri::HTML(open(wp_path, 'User-Agent' => 'Nooby'))
-if !wppage.css('.preview').size
-	puts '404!'
+def choose_wp redditscrape
+	a = rand(0..redditscrape.css('.thing').size)
+	wallobj = redditscrape.css('.thing')[a]
+	wp_path = wallobj.css('.bylink').first.attributes['href'].value
+	wppage = Nokogiri::HTML(open(wp_path, 'User-Agent' => 'Nooby'))
+	if !wppage.css('.preview').size
+		choose_wp redditscrape
+	else
+		wp_link = wppage.css('.preview').first.attributes['src'].value
+		set_wp wp_link
+	end
 end
 
-wp_link = wppage.css('.preview').first.attributes['src'].value
+def set_wp wp_link
+	open(wp_link) { |f|
+		File.open('wallpaper.png','wb')  do |file|
+			file.puts f.read
+		end
+	}
+	puts system 'gsettings set org.gnome.desktop.background picture-uri file://' + Dir.pwd + '/wallpaper.png'	
+end
 
-open(wp_link) { |f|
-	File.open('wallpaper.png','wb')  do |file|
-		file.puts f.read
-	end
-}
-
-puts system 'gsettings set org.gnome.desktop.background picture-uri file://'+Dir.pwd+'/wallpaper.png'
+choose_wp redditscrape
 
